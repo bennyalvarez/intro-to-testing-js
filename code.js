4a9ee84df47085d23bffdd824da12cffda493d2a
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-04-07 19:20:56 -0500

    completed exercise step 8 of intro to testing

--- code.js
+++ code.js
@@ -4,6 +4,9 @@ function helloWorld() {
 }
 
 // sayHello function
-function sayHello(input) {
-    return "Hello, " + input + "!";
+function sayHello(x) {
+    if ((typeof x === 'string' && x.trim().length > 0) || typeof x === 'number' ) {
+        return "Hello, " + x + "!";
+    }
+    return 'Hello, World!';
 }
